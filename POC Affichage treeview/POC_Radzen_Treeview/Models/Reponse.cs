6bac0fce537fda945712ce34d85787df3ec72cfb
mathieuKero <m.keromnes@iia-laval.fr>
2020-11-02 18:47:45 +0100

    POC Mise en place de l'affichage par RADZEN

---
+++ POC Affichage treeview/POC_Radzen_Treeview/Models/Reponse.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POC_Radzen_Treeview.Models
+{
+    public class Reponse
+    {
+        #region Properties
+
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [JsonProperty("Texte")]
+        public string Texte { get; set; }
+
+        public Question Question { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public Reponse()
+        {
+
+        }
+
+        public Reponse(int id, string texte)
+        {
+            Id = id;
+            Texte = texte;
+        }
+
+
+        #endregion
+    }
+}
