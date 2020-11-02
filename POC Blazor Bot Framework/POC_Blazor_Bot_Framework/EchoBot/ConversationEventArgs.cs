fb88f5e668b8176b66c400f881acdc1984bf023b
mathieuKero <m.keromnes@iia-laval.fr>
2020-11-02 18:54:28 +0100

    POC blazor + bot framwork

---
+++ POC Blazor Bot Framework/POC_Blazor_Bot_Framework/EchoBot/ConversationEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POC_Blazor_Bot_Framework.EchoBot
+{
+    public class ConversationEventArgs : EventArgs
+    {
+        public Activity Activity { get; set; }
+    }
+}
