b64444bd647ebbb2a4d0ad43d4e57892d65bb290
chockella <jorisguerrier@gmail.com>
2020-11-02 18:37:08 +0100

    POC lecture json

---
+++ POC lecture json/POC lecture json/QuestionReponse.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC_lecture_json
+{
+    class QuestionReponse
+    {
+        #region Properties
+
+        [JsonProperty("IdQuestionReponse")]
+        public int IdQuestionReponse { get; set; }
+
+        [JsonProperty("LabelQuestion")]
+        public string LabelQuestion { get; set; }
+
+        [JsonProperty("LabelReponse")]
+        public string LabelReponse { get; set; }
+
+        [JsonProperty("IdPosition")]
+        public int IdPosition { get; set; }
+
+        [JsonProperty("IdQRParent")]
+        public int? IdQRParent { get; set; }
+
+        public QuestionReponse PreviousQuestion { get; set; }
+
+        public List<QuestionReponse> ListQuestionReponses { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public QuestionReponse()
+        {
+
+        }
+
+        public QuestionReponse(int idQuestionReponse, string labelQuestion, string labelReponse, int? idQRParent, int idPosition)
+        {
+            IdQuestionReponse = idQuestionReponse;
+            LabelQuestion     = labelQuestion;
+            LabelReponse      = labelReponse;
+            IdQRParent        = idQRParent;
+            IdPosition        = idPosition;            
+        }
+
+        #endregion
+
+        #region Methods
+            
+
+
+
+
+        #endregion
+
+    }
+}
---
+++ POC lecture json/POC lecture json/Reponse.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC_lecture_json
+{
+    class Reponse
+    {
+        #region Properties
+
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [JsonProperty("Texte")]
+        public string Texte { get; set; }
+
+        public QuestionReponse Question { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public Reponse()
+        {
+
+        }
+
+        public Reponse(int id, string texte)
+        {
+            Id = id;
+            Texte = texte;
+        }
+
+
+        #endregion
+    }
+}
