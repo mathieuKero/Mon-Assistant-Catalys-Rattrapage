eb5340cb3256cd74425e9381e4cc69d93db5abf8
chockella <jorisguerrier@gmail.com>
2020-12-04 15:37:29 +0100

    Initialisation de la classe de tests pour QuestionnaireService

---
+++ POC Old Version test treeview/Mon_Assistant_Catalys.Unit_Tests/Services/QuestionnaireServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mon_Assistant_Catalys.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mon_Assistant_Catalys.Web.Services.Tests
+{
+    [TestClass()]
+    public class QuestionnaireServiceTest
+    {
+        [TestMethod()]
+        public void QuestionnaireServiceTests()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void displayTreeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateJsonFilesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConstructTreeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetQuestionnaireTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ POC Old Version test treeview/Mon_Assistant_Catalys_Tests/QuestionnaireServiceTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mon_Assistant_Catalys_Tests
+{
+    [TestClass]
+    public class QuestionnaireServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
